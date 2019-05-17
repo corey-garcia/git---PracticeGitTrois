d3bac8c4b62ba359aef765dc298f13c9b2018e6f
Corey <Corey@Coreys-MacBook-Air.local>
2019-05-17 15:31:25 -0600

    Initial commit

---
+++ PracticeGitTrois/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeGitTrois
+//
+//  Created by Corey  on 5/17/19.
+//  Copyright © 2019 Corey . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Hello World
+    }
+
+
+}
+
